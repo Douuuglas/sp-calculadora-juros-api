93d8befd69be4d3d65577272b19800d0ca88f8ea
Douglas Cassiano de Moura <douuuglas@gmail.com>
2021-03-01 13:01:55 -0300

    Criando teste de integração do CalculaJurosController

---
+++ SP.CALCULADORA.JUROS/WebApi.IntegrationTests/Controllers/CalculaJurosControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Dtos;
+using Xunit;
+
+namespace WebApi.IntegrationTests.Controllers
+{
+    public class CalculaJurosControllerTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public CalculaJurosControllerTests()
+        {
+            _httpClient = new WebApplicationFactory<Startup>().CreateClient();
+        }
+
+        [Fact]
+        public async Task DadaRequiscaoGetDeveRetornarUmPorcento()
+        {
+            var endpoint = "CalculaJuros";
+            var request = new CalculaJurosCompostosDto() { ValorInicial = 100, TempoEmMeses = 3  };
+            var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+
+            var response = await _httpClient.PostAsync(endpoint, content);
+            var result = Convert.ToDecimal(await response.Content.ReadAsStringAsync());
+
+            response.StatusCode.Should().Be(200);
+            result.Should().Be(103.03m);
+        }
+    }
+}
